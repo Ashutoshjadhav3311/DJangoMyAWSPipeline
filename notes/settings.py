bc4c4b51e3da18e480cba70c2a5abbcd84dbe984
Ashutosh Jadhav <104265231+Ashutoshjadhav3311@users.noreply.github.com>
2023-12-10 17:29:46 +0000

    Update settings.py

--- notes/settings.py
+++ notes/settings.py
@@ -10,12 +10,74 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import boto3
+import json
+import base64
+from botocore.exceptions import ClientError
 from pathlib import Path
-from notes.getcredentials.py import getdjangokey,getdbkey
+#from notes.getcredentials.py import getdjangokey,getdbkey
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
-
+ 
+def getdjangokey():
+ 
+# AWS Secrets Manager to use AWS credentials & Django secret Key stored there
+    secret_name = "sonarcloudDjangoSecreat"  
+    region_name="us-west-2"
+ 
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+ 
+    try:
+        get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+    except ClientError as e:
+        # Handle the exception based on the error code
+        raise e
+    else:
+        # Decrypts secret using the associated KMS CMK
+        if 'SecretString' in get_secret_value_response:
+            secret = get_secret_value_response['SecretString']
+            djangosecret_dict = json.loads(secret)
+#            print("Retrieved secret:", secret_dict)  # Testing line
+            return djangosecret_dict
+        else:
+            decoded_binary_secret = base64.b64decode(get_secret_value_response['SecretBinary'])
+            return json.loads(decoded_binary_secret)    
+ 
+def getdbkey():
+ 
+# AWS Secrets Manager to use AWS credentials & Django secret Key stored there
+    secret_name = "databaseSecret"  
+    region_name="us-west-2"
+ 
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+ 
+    try:
+        get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+    except ClientError as e:
+        # Handle the exception based on the error code
+        raise e
+    else:
+        # Decrypts secret using the associated KMS CMK
+        if 'SecretString' in get_secret_value_response:
+            dbsecret = get_secret_value_response['SecretString']
+            dbsecret_dict = json.loads(dbsecret)
+#            print("Retrieved secret:", secret_dict)  # Testing line
+            return dbsecret_dict
+        else:
+            decoded_binary_secret = base64.b64decode(get_secret_value_response['SecretBinary'])
+            return json.loads(decoded_binary_secret)    
+ 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
