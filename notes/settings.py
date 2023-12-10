d987bf6d3ce6784f022fc161f3fe410b7e5e6af6
Ashutosh Jadhav <104265231+Ashutoshjadhav3311@users.noreply.github.com>
2023-12-10 17:08:54 +0000

    Update settings.py

--- notes/settings.py
+++ notes/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-from .getcredentials.py import getdjangokey,getdbkey
+from notes.getcredentials.py import getdjangokey,getdbkey
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
